62103c2a91ea841a9614a519ee65dabed217d4be
Jhon Esparragoza <jhonantonyesp@gmail.com>
2024-05-27 20:31:28 +0200

    este

--- index.js
+++ index.js
@@ -1,43 +1,57 @@
-const express = require('express');
+const express = require('express')
 const bodyParser = require('body-parser');
+require('dotenv').config()
 
-const { getStoredPosts, storePosts } = require('./data/posts');
-
-const app = express();
-
+const app = express()
+const mongoose = require('mongoose');
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  // Attach CORS headers
-  // Required when using a detached backend (that runs on a different domain)
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
+    // Attach CORS headers
+    // Required when using a detached backend (that runs on a different domain)
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
+
+app.get('/', (req, res) =>{
+    res.json({mssg: 'Welcome to the app'})
+} )
+
+const Schema = mongoose.Schema
+
+const notaSchema = new Schema({
+    autor: {type: String, required: true},
+    body: {type: String, required: true}
+})
+
+Nota = mongoose.model('Nota', notaSchema)
 
 app.get('/posts', async (req, res) => {
-  const storedPosts = await getStoredPosts();
-  // await new Promise((resolve, reject) => setTimeout(() => resolve(), 1500));
-  res.json({ posts: storedPosts });
-});
-
-app.get('/posts/:id', async (req, res) => {
-  const storedPosts = await getStoredPosts();
-  const post = storedPosts.find((post) => post.id === req.params.id);
-  res.json({ post });
-});
+    const storedPosts = await Nota.find({});
+    // await new Promise((resolve, reject) => setTimeout(() => resolve(), 1500));
+    res.json({ posts: storedPosts });
+  });
 
 app.post('/posts', async (req, res) => {
-  const existingPosts = await getStoredPosts();
-  const postData = req.body;
-  const newPost = {
-    ...postData,
-    id: Math.random().toString(),
-  };
-  const updatedPosts = [newPost, ...existingPosts];
-  await storePosts(updatedPosts);
-  res.status(201).json({ message: 'Stored new post.', post: newPost });
-});
+    const postData = req.body;
+    try{
+        const guardarNota = await Nota.create(postData);
+        res.status(200).json(guardarNota)
+    }catch(error){
+        res.status(400).json({error: error.message})
+    }
+})
 
-app.listen(8080);
+mongoose.connect(process.env.MONGO_URI)
+    .then(()=>{
+        app.listen(3000, ()=>{
+            console.log("listening on port ", 3000);
+        })
+    })
+    .catch((error) =>{
+        console.log(error);
+    })
+
+module.exports.app
